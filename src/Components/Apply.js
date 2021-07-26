62109aa9f91e9ce1d93899bde7cd41ac6be6a12b
DumboKim <rlawlsgus97@gmail.com>
2021-07-26 11:36:26 +0900

    real finish

--- src/Components/Apply.js
+++ src/Components/Apply.js
@@ -11,35 +11,35 @@ export default function Apply() {
         </div>
 
         <div id='apply_input'>
-          <div id='apply_input1'>
+          <div class='apply_input' id='apply_input1'>
             <label><span>*</span>이름</label>
             <br/>
             <input placeholder='이름을 입력해주세요' />
           </div>
-          <div id='apply_input2'>
+          <div class='apply_input' id='apply_input2'>
             <label>연락처</label>
             <br/>
             <input placeholder='연락처를 입력해주세요' />
           </div>
           <br/>
-          <div id='apply_input3'>
+          <div class='apply_input' id='apply_input3'>
             <label>차량명</label>
             <br/>
             <input placeholder='차량명을 입력해주세요' />
           </div>
-          <div id='apply_input4'>
+          <div class='apply_input' id='apply_input4'>
             <label>지역</label>
             <br/>
             <input placeholder='지역을 입력해주세요' />
           </div>
           <br/>
-          <div id='apply_input_bottom'>
+          <div class='apply_input' id='apply_input_bottom'>
             <div id='apply_input5'>
               <label>대여 일자</label>
               <br/>
               <input placeholder='예상 대여일자를 입력해주세요' />
             </div>
-            <div id='apply_input6'>
+            <div class='apply_input' id='apply_input6'>
               <label>요청 사항</label>
               <br/>
               <input placeholder='요청사항을 입력해주세요' />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Header.css'
 import savecar_logo from '../img/savecar_logo.png'
 
 export default function Header() {
-
+  
   useEffect(() => {
-    const header = document.getElementById('header_wrapper');
+
+    let header = document.getElementById('header_wrapper');
+
     window.addEventListener('scroll', ()=> {
       if(window.scrollY > 50) {
-        header.id='header_wrapper_scroll'
+        header?.setAttribute('id', 'header_wrapper_scroll')
       } else {
-        header.id='header_wrapper'
+        header?.setAttribute('id', 'header_wrapper')
       }
     })
+
   })
+
+  const [menuStatus, setmenuStatus] = useState(false)
+
+  const menuClick = () => {
+    menuStatus? setmenuStatus(false) : setmenuStatus(true);
+  }
+
+  const menuOpen = () => {
+    return (
+    <ul id='nav_bar'>
+      <li>HOME</li>
+      <li>월렌트+12개월렌트</li>
+      <li>누구나장기렌트</li>
+      <li>캠핑카렌트</li>
+      <li>캠핑카 장기 &amp; 쉐어링</li>
+      <li>한눈에 보는 세이브카 가격표</li>
+    </ul>
+    )
+  }
+  
   
 
   return (
     <div id='header_wrapper'>
+      <div id='nav_btn'>
+        <ul id='nav_bar_wrapper'>
+          {menuStatus? <li id='menu_btn' onClick={() => {menuClick()}}>MENU<button>X</button></li> : <li onClick={() => {menuClick()}}>MENU</li>}
+          {menuStatus? menuOpen(): null}
+        </ul>
+      </div>
       <div id='header_logo'><a href='/'><img src={savecar_logo} alt='header_logo'/></a></div>
       <div id='header_btn'>
         <a href='/'><button id='header_home'>HOME</button></a>
@@ -28,5 +57,6 @@ export default function Header() {
         <a href='/'><button id='header_price_list'>한눈에 보는 세이브카 가격표</button></a>
       </div>
     </div>
+    
   )
 }
